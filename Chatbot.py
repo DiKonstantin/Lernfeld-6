82b7a05ef6a5aaf1a1572c42cb98a065b1931e9a
dikonstantin <konstibg@gmail.com>
2024-04-22 12:42:22 +0200

    GUI THrow

---
+++ Chatbot.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+
+root = Tk()
+
+root.geometry('500x570+100+30')
+root.title('Chatbot created by StrAltDefeat')
+root.config(bg='grey')
+root.resizable(False,False)
+
+logoPic = PhotoImage(file='Design ohne Titel.png')
+logoPicLabel = Label(root, image=logoPic, bg='white')
+logoPicLabel.pack(pady=5)
+
+centerFrame = Frame(root)
+centerFrame.pack()
+
+scrollbar = Scrollbar(centerFrame)
+scrollbar.pack(side=RIGHT)
+
+textarea = Text(centerFrame, font=('arial',20,'bold'),height=5,yscrollcommand=scrollbar.set, wrap='word')
+textarea.pack(side=LEFT)
+scrollbar.config(command=textarea.yview)
+
+questionField = Entry(centerFrame, font=('arial',20,'bold'))
+questionField.pack(pady=10,fill=X)
+
+askButton = Button(root)
+askButton.pack()
+askButton['text'] = 'Submit'
+
+
+root.mainloop()
