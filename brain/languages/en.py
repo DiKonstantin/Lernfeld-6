9c000042960443f9b11aaece3795ec9713f9c7aa
kinga <kinjer94@gmail.com>
2024-04-24 16:26:36 +0200

    en übersetzung

--- brain/languages/en.py
+++ brain/languages/en.py
@@ -5,3 +5,14 @@ messages = {
     "end_conversation": "Thanks for Using Strg+Alt+DefeatBot",
     "enter_language": "Please enter your languagecode [en, de]:",
 }
+questions = {
+    1: "Which data protection policies and procedures have been implemented?",
+    2: "How are the personal data managed and stored?",
+    3: "Who is the data protection officer for my data?",
+    4: "Which data are included?",
+    5: "What measures are taken to protect this data?",
+    6: "How long will my data be stored?",
+    7: "How is data deletion handled?",
+    8: "Who has access to and insight into my data?",
+    9: "Are data protection trainings offered, and what would they entail?"
+}
