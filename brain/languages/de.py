180af9ff8d1dad67f8f134aa7db6878337b8d840
dikonstantin <konstibg@gmail.com>
2024-04-24 12:30:35 +0200

    Gui Logo Picture

--- brain/languages/de.py
+++ brain/languages/de.py
@@ -5,3 +5,12 @@ messages = {
     "end_conversation": "Danke, dass du Strg+Alt+DefeatBot genutzt hast.",
     "enter_language": "Bitte gib den gewünschten Sprachcode ein [de, en]:",
 }
+questions = {
+    1 : "Welche Datenschutzrichtlinien und -verfahren wurden implementiert?",
+    2 : "Wie werden die personenbezogenen Daten verwaltet und gespeichert?",
+    3 : "Wer ist der Datenschutzbeauftragte für meine Daten?",
+    4 : "Welche Daten werden miteinbegriffen?",
+    5 : "Welche Maßnahmen werden zum Schutz dieser Daten ergriffen?",
+    6 : "Wie lange werden meine Daten gespeichert?",
+
+}
