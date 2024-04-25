65b32f1f8ba25fa6e1facfc40d1078c4b62c8bff
bitbike <9683666+bitbike@users.noreply.github.com>
2024-04-25 12:06:20 +0200

    fix stop-event

--- brain/Cortex.py
+++ brain/Cortex.py
@@ -8,13 +8,13 @@ class Cortex:
     """
     The Main Brain Class
 
-    This Class must be loaded as object and will hande the requests and provides the answers. The Class will log
+    This Class must be loaded as object and will handle the requests and provides the answers. The Class will log
     every Instance with a timestamped file.
 
     Attributes:
         message_queue (asyncio.Queue): Queue to send messages to.
         answer_queue (asyncio.Queue): Queue to read answers from.
-
+        stop_event (asyncio.Event): Event to signal the shutdown of the application.
     """
 
     def __init__(self):
@@ -28,6 +28,7 @@ class Cortex:
         """
         self.message_queue = asyncio.Queue()
         self.answer_queue = asyncio.Queue()
+        self.stop_event = asyncio.Event()
 
         current_timestamp = int(time.time())
         filename = "log_" + str(current_timestamp) + ".txt"
@@ -35,8 +36,8 @@ class Cortex:
 
     async def start_session(self):
         """
-        This method will start the main loop of the Cortex Class. This loop will handly asynchronosly the
-        message_qeueue and will then send an Anwer into the answer_queue.
+        This method will start the main loop of the Cortex Class. This loop will handle asynchronously the
+        message_queue and will then send an Answer into the answer_queue.
 
         """
         conversation_running = True
@@ -44,20 +45,21 @@ class Cortex:
         await self.send_message(language.get_message("announce_quit"))
 
         while conversation_running:
-            await asyncio.sleep(0.1)  # small delay to not have an CPU-overloading loop
+            await asyncio.sleep(0.1)  # small delay to not have a CPU-overloading loop
             if not self.message_queue.empty():
-                next_message = await self.message_queue.get()  # get the next message from the stack. this loop will handle each message one by one.
+                next_message = await self.message_queue.get()  # get the next message from the stack. This loop will handle each message one by one.
 
                 # handle now the "quit" message
                 if next_message == "quit":
                     conversation_running = False
+
                 # now handle all other messages incoming
                 else:
                     ##### REPLACE THE CODE HERE FOR YOUR LOGIC #####
                     await self.send_answer("You entered " + next_message)
 
-        await self.send_answer(language.get_message("quit"))
-        self.file.close()
+        # await self.send_answer(language.get_message("end_conversation"))
+        self.stop_event.set()  # Set the stop event to signal other tasks to stop
 
     async def send_message(self, message):
         """
@@ -67,7 +69,6 @@ class Cortex:
         """
         await self.message_queue.put(message)
         self.file.write(message + "\n")
-        self.file.flush()
 
     async def send_answer(self, message):
         """
@@ -77,7 +78,6 @@ class Cortex:
         """
         await self.answer_queue.put(message)
         self.file.write(message + "\n")
-        self.file.flush()
 
     async def get_next_answer(self):
         """
@@ -91,14 +91,17 @@ class Cortex:
 
 async def input_loop(brain):
     loop = asyncio.get_event_loop()
-    while True:
+    while not brain.stop_event.is_set():  # Continue listening for input until the stop event is set
         user_input = await loop.run_in_executor(None, input, "")
         print()
         await brain.send_message(user_input)
 
 
+
 async def output_loop(brain):
     while True:
+        if brain.stop_event.is_set():  # Check if the stop event is set
+            break
         answer = await brain.get_next_answer()
         if answer:
             print("Received answer:", answer)
@@ -111,9 +114,8 @@ async def main():
     asyncio.create_task(input_loop(brain))
     asyncio.create_task(output_loop(brain))
 
-    # Create an event to keep the main coroutine running indefinitely
-    await asyncio.Event().wait()
-
+    # Wait until the stop event is set before exiting
+    await brain.stop_event.wait()
 
 if __name__ == "__main__":
     asyncio.run(main())
